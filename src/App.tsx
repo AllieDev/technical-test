5c67990198d54e1fb7a1cf16c50c3731d05040b3
Ali Sultani <sultanialiasghar@gmail.com>
2023-02-17 16:31:38 +1300

    add redux

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import Footer from "./components/layouts/Footer";
 import Map from "./components/maps/Map";
 import Loading from "./components/UI/Loading";
 import "./App.css";
+import { RootState } from "./store";
+import { useSelector } from "react-redux";
 
 function App() {
+  const mapLoading = useSelector(
+    (state: RootState) => state.sliceState.loading
+  );
   return (
     <div className="App">
       <Header />
-      <Loading />
+      {mapLoading && <Loading />}
       <Map />
       <Footer />
     </div>
