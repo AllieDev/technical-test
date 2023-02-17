a34efc076b6655af6db2618ecd47b5ac5ab6402e
Ali Sultani <sultanialiasghar@gmail.com>
2023-02-17 14:18:13 +1300

    Update footer copyright

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,4 +1,5 @@
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 export default function Footer() {
   return (
@@ -6,7 +7,13 @@ export default function Footer() {
       sx={{
         height: 100,
         backgroundColor: "primary.dark",
+        display: "grid",
+        placeItems: "center",
       }}
-    />
+    >
+      <Typography variant="body1" component="span" sx={{ color: "#fff" }}>
+        © COPYRIGHT ALI ASGHAR SULTANI - 2023
+      </Typography>
+    </Box>
   );
 }
