dc02fda249e84124f0b8915977ac3b402d29292c
Ali Sultani <sultanialiasghar@gmail.com>
2023-02-17 17:01:57 +1300

    Test loading states

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,7 @@ import { describe, expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { store } from "./store";
 import { Provider } from "react-redux";
+import { setLoading } from "./slice";
 
 vi.mock("./components/maps/Map", () => {
   return {
@@ -18,5 +19,22 @@ describe("#App", () => {
       </Provider>
     );
     expect(container.textContent).toMatchSnapshot();
+  });
+  it("should display a loading", () => {
+    const { container } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    expect(container.textContent).to.include("Loading");
+  });
+  it("should not display a loading", () => {
+    store.dispatch(setLoading(false));
+    const { container } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    expect(container.textContent).not.to.include("Loading");
   });
 });
